43599f25e6348262e39eab85eab78369aaa053e6
AlexandrDubovchenko <sasha.dubovchenko@gmail.com>
2025-01-10 17:12:47 +0200

    logs

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -8,18 +8,19 @@ const token = process.env.TELEGRAM_BOT_TOKEN ?? ''
 export const bot = new Telegraf(token);
 
 bot.command('start', async (ctx) => {
+  console.log("Bot start command");
   const chatId = ctx.chat.id;
   const existingChat = await chatRepository.getChatById(chatId);
   if (!existingChat) {
     try {
       await chatRepository.createChat(chatId, 'REGISTRATION');
+      ctx.reply('Введите монобанк апи ключ')
     } catch {
       console.error('ERROR: bot/index.ts');
     }
   } else {
     await chatRepository.updateStatus(chatId, 'WAIT_COMMAND')
   }
-  ctx.reply('Введите монобанк апи ключ')
 })
 
 bot.on(message("text"), async ctx => {
@@ -27,7 +28,8 @@ bot.on(message("text"), async ctx => {
   const message = ctx.message
   const apiKey = message.text
   const chat = await chatRepository.getChatById(chatId)
-
+  console.log("Message in bot ", message);
+  
   if (chat?.status === 'REGISTRATION') {
     await ctx.sendMessage(`Получаем данные пользователя...`);
     try {
