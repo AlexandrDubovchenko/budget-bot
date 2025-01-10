80c0cb8a613c822caee5c7cda5fd2bfc29299ce4
AlexandrDubovchenko <sasha.dubovchenko@gmail.com>
2025-01-10 17:16:59 +0200

    fix bot

--- src/app/api/telegram/webhook/route.ts
+++ src/app/api/telegram/webhook/route.ts
@@ -4,6 +4,6 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(request: NextRequest) {
   const body = await request.json();
   console.log('New telegram request ', JSON.stringify(body))
-  bot.handleUpdate(body);
+  await bot.handleUpdate(body);
   return NextResponse.json({ message: "ok" }, { status: 200 });
 }
